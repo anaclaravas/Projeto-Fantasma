a415fc70722b293f8feb479a5c3f29b3f0529399
anaclaravas <167478706+anaclaravas@users.noreply.github.com>
2024-05-07 00:57:01 -0300

    Add files via upload

---
+++ Análise_3.R
@@ -0,0 +1,83 @@
+#Projeto Fantasma
+#Análise 3
+
+#Top 3 terrenos mais frequentes pela ativação da armadilha
+#Quais são os 3 tipos de terrenos mais frequentes
+#Quais armadilhas funcionam de primeira neles
+
+
+#Pacotes
+install.packages("readr")
+library(readr)
+install.packages("tidyr")
+library(tidyr)
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("dplyr")
+library(dplyr)
+install.packages("ggplot2")
+library(ggplot2)
+
+#Padronização
+
+cores_estat <- c("#A11D21", "#003366", "#CC9900", "#663333", "#FF6600",
+                 "#CC9966", "#999966", "#006606", "#008091", "#041835",
+                 "#666666")
+
+theme_estat <- function(...){theme <- ggplot2::theme_bw() + ggplot2::theme(axis.title.y = ggplot2::element_text(colour="black",
+                                                                                                                size = 12), axis.title.x = ggplot2::element_text(colour="black", size = 12),
+                                                                           axis.text = ggplot2::element_text(colour="black", size=9.5), 
+                                                                           panel.border = ggplot2::element_blank(),
+                                                                           axis.line = ggplot2::element_line(colour="black"), legend.position = "top",...)
+return(list(theme, scale_fill_manual(values=cores_estat), 
+            scale_colour_manual(values=cores_estat)))}
+
+#Terrenos mais frequentes
+
+top3<-as.data.frame(banco_final$setting_terrain)
+
+colnames(top3)[1]<-"terreno"
+
+tabela3<-table(top3)
+
+top3<-as.data.frame(tabela3)
+
+top3<-top3[-c(1, 2, 3, 4, 6, 7, 8, 9, 10, 12, 13, 14), ]
+
+
+#Separando as variáveis de interesse em data frames
+
+df<-as.data.frame(banco_final$setting_terrain)
+df<-df%>%
+  dplyr::mutate(banco_final$trap_work_first)
+
+colnames(df)[1]<-"terreno"
+colnames(df)[2]<-"armadilha"
+
+urban<-subset(df, terreno=="Urban")
+rural<-subset(df, terreno=="Rural")
+forest<-subset(df, terreno=="Forest")
+
+urban<-as.data.frame(table(urban))
+rural<-as.data.frame(table(rural))
+forest<-as.data.frame(table(forest))
+
+#Construção dos gráficos
+
+trans_drv <- mpg %>%
+  mutate(trans = case_when(trans %>% str_detect("auto") ~ "auto",
+    trans %>% str_detect("manual") ~ "manual")) %>%
+  group_by(trans, drv) %>%
+  summarise(freq = n()) %>%
+  mutate(freq_relativa = round(freq/sum(freq) * 100,1))
+porcentagens<-str_c(trans_drv$freq_relativa, "%") %>% str_replace("
+\\.", ",")
+legendas <- str_squish(str_c(trans_drv$freq, "(", porcentagens, ")"))
+ggplot(trans_drv) +
+  aes(x = fct_reorder(trans, freq, .desc = T), y = freq,
+    fill = drv, label = legendas) +
+  geom_col(position = position_dodge2(preserve = "single", padding = 0)) +
+  geom_text(position = position_dodge(width = .9),
+    vjust = -0.5, hjust = 0.5,
+    size = 3) + labs(x = "Transmissão", y = "Frequência") + theme_estat()
+ggsave("colunas-bi-freq.pdf", width = 158, height = 93, units = "mm")
