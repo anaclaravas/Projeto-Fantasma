16134159dc3c8bd289861f9be29045b9e6c6941b
anaclaravas <167478706+anaclaravas@users.noreply.github.com>
2024-05-21 22:22:14 -0300

    Análise_5.R

--- Análise_5.R
+++ Análise_5.R
@@ -1,12 +1,9 @@
 #Análise 5
-#Variação da nota de engajamento pelo personagem que conseguiu capturar o monstro
+#Variação do engajamento pelo personagem que conseguiu capturar o monstro
 
 #Pacotes
 install.packages("tidyverse")
 library(tidyverse)
-install.packages("tibble")
-library(tibble)
-
 
 #Padronização ESTAT
 
@@ -29,14 +26,14 @@ df_fred<-as.data.frame(banco_final$caught_fred)
 df_fred<-df_fred%>%
   dplyr::mutate(banco_final$engagement)
 
-colnames(df_fred)[1]<-"fred"
+colnames(df_fred)[1]<-"cap"
 colnames(df_fred)[2]<-"eng"
 
-df_fred<-df_fred[df_fred$fred == "TRUE",]
+df_fred<-df_fred[df_fred$cap == "TRUE",]
 
 df_fred<-na.omit(df_fred)
 
-df_fred$fred<-NULL
+df_fred$cap<-"Fred"
 
 
 #Daphne
@@ -46,14 +43,14 @@ df_daphne<-as.data.frame(banco_final$caught_daphnie)
 df_daphne<-df_daphne%>%
   dplyr::mutate(banco_final$engagement)
 
-colnames(df_daphne)[1]<-"daphne"
+colnames(df_daphne)[1]<-"cap"
 colnames(df_daphne)[2]<-"eng"
 
-df_daphne<-df_daphne[df_daphne$daphne == "TRUE",]
+df_daphne<-df_daphne[df_daphne$cap == "TRUE",]
 
 df_daphne<-na.omit(df_daphne)
 
-df_daphne$daphne<-NULL
+df_daphne$cap<-"Daphne"
 
 #Velma
 
@@ -62,14 +59,14 @@ df_velma<-as.data.frame(banco_final$caught_velma)
 df_velma<-df_velma%>%
   dplyr::mutate(banco_final$engagement)
 
-colnames(df_velma)[1]<-"velma"
+colnames(df_velma)[1]<-"cap"
 colnames(df_velma)[2]<-"eng"
 
-df_velma<-df_velma[df_velma$velma == "TRUE",]
+df_velma<-df_velma[df_velma$cap == "TRUE",]
 
 df_velma<-na.omit(df_velma)
 
-df_velma$velma<-NULL
+df_velma$cap<-"Velma"
 
 
 
@@ -81,14 +78,14 @@ df_shaggy<-as.data.frame(banco_final$caught_shaggy)
 df_shaggy<-df_shaggy%>%
   dplyr::mutate(banco_final$engagement)
 
-colnames(df_shaggy)[1]<-"shaggy"
+colnames(df_shaggy)[1]<-"cap"
 colnames(df_shaggy)[2]<-"eng"
 
-df_shaggy<-df_shaggy[df_shaggy$shaggy == "TRUE",]
+df_shaggy<-df_shaggy[df_shaggy$cap == "TRUE",]
 
 df_shaggy<-na.omit(df_shaggy)
 
-df_shaggy$shaggy<-NULL
+df_shaggy$cap<-"Salsicha"
 
 
 #Scooby
@@ -98,14 +95,14 @@ df_scooby<-as.data.frame(banco_final$caught_scooby)
 df_scooby<-df_scooby%>%
   dplyr::mutate(banco_final$engagement)
 
-colnames(df_scooby)[1]<-"scooby"
+colnames(df_scooby)[1]<-"cap"
 colnames(df_scooby)[2]<-"eng"
 
-df_scooby<-df_scooby[df_scooby$scooby == "TRUE",]
+df_scooby<-df_scooby[df_scooby$cap == "TRUE",]
 
 df_scooby<-na.omit(df_scooby)
 
-df_scooby$scooby<-NULL
+df_scooby$cap<-"Scooby"
 
 
 #Não foi pego
@@ -115,14 +112,14 @@ df_not<-as.data.frame(banco_final$caught_not)
 df_not<-df_not%>%
   dplyr::mutate(banco_final$engagement)
 
-colnames(df_not)[1]<-"not"
+colnames(df_not)[1]<-"cap"
 colnames(df_not)[2]<-"eng"
 
-df_not<-df_not[df_not$not == "TRUE",]
+df_not<-df_not[df_not$cap == "TRUE",]
 
 df_not<-na.omit(df_not)
 
-df_not$not<-NULL
+df_not$cap<-"Não foi pego"
 
 #Pego por outro personagem
 
@@ -131,13 +128,26 @@ df_other<-as.data.frame(banco_final$caught_other)
 df_other<-df_other%>%
   dplyr::mutate(banco_final$engagement)
 
-colnames(df_other)[1]<-"other"
+colnames(df_other)[1]<-"cap"
 colnames(df_other)[2]<-"eng"
 
-df_other<-df_other[df_other$other == "TRUE",]
+df_other<-df_other[df_other$cap == "TRUE",]
 
 df_other<-na.omit(df_other)
 
-df_other$other<-NULL
+df_other$cap<-"Outro"
 
 #Juntando os dados
+
+data<-bind_rows(df_fred, df_daphne, df_velma, 
+                df_shaggy, df_scooby, df_not, df_other)
+
+#Gráfico----
+
+ggplot(data) +
+  aes(x = reorder(cap, eng, FUN = median), y = eng) +
+  geom_boxplot(fill = c("#A11D21"), width = 0.5) +
+  stat_summary(fun = "mean", geom = "point", shape = 23, size = 3, fill = "white") +
+  labs(x = "Personagem que capturou o monstro", y = "Engajamento") +
+  theme_estat()
+ggsave("plot_analise5.pdf", width = 158, height = 93, units = "mm")
